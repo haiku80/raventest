54cf14d1ac4739dbfa62a5c8de9baaf439fef579
Sig. Tolleranza <haiku80@gmail.com>
2012-11-08 18:04:47 +0100

    Aggiunta bozza sezione Blog

---
+++ RavenTest/Controllers/BlogController.cs
@@ -0,0 +1,35 @@
+﻿using Raven.Client;
+using RavenTest.Controllers.Base;
+using RavenTest.Models;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace RavenTest.Controllers
+{
+  public class BlogController : BaseController
+  {
+    public BlogController(IDocumentSession documentSession) : base(documentSession) { }
+
+    public ActionResult Index()
+    {
+      var blogPosts = RavenSession.Query<BlogPost>().OrderByDescending(b => b.Created);
+
+      return View(blogPosts);
+    }
+
+    public ActionResult Create()
+    {
+      return View(new BlogPost());
+    }
+
+    [HttpPost]
+    public ActionResult Create(BlogPost blogPost)
+    {
+      RavenSession.Store(blogPost);
+      RavenSession.SaveChanges();
+
+      return RedirectToAction("Index");
+    }
+
+  }
+}
---
+++ RavenTest/Models/BlogPost.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
+
+namespace RavenTest.Models
+{
+  public class BlogPost
+  {
+    [Required]
+    public string Id { get; set; }
+    [Required]
+    public string Title { get; set; }
+    [Required]
+    [AllowHtml]
+    [DataType(DataType.MultilineText)]
+    public string Body { get; set; }
+    [Required]
+    [Display(Name = "Created on")]
+    public DateTime Created { get; set; }
+    [Display(Name = "Last Modified")]
+    public DateTime? LastModified { get; set; }
+
+    public BlogPost()
+    {
+      Created = DateTime.Now;
+    }
+  }
+}
